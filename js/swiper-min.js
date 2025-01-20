c49fd8dddda0164e75c345784fef38ea72c9bfd6
gvadiko2004 <archilbigvava@gmail.com>
2025-01-20 18:54:34 +0300

    commit

---
+++ js/swiper-min.js
@@ -0,0 +1,11 @@
+const swiperTesti = new Swiper(".swiper-testimonials", {
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 52,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
